84cddc274f55e1fbafa9e21e83e2fd1f0c085fb0
boo1ean <boo1ean0807@gmail.com>
2015-09-18 13:50:22 +0300

    Add basic reporter implementation

---
+++ src/reporter.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var _ = require('lodash');
+var nodemailer = require('nodemailer');
+var log = require('./log');
+
+_.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
+
+module.exports = class Reporter {
+	constructor (opts) {
+		this.mailOptions = opts.mailOptions;
+		this.mailer = nodemailer.createTransport(opts.mailer);
+	}
+
+	report (repo, errors) {
+		log.info('Sending error message for %s repo there are %s wrong commits', repo, errors.length);
+
+		return new Promise((resolve, reject) => {
+			var data = {
+				repo: repo,
+				username: errors[0].author.username,
+				commits: errors
+			};
+
+
+			var mail = _.clone(this.mailOptions);
+
+			mail.subject = _.template(mail.subject)(data);
+			mail.html = _.template(mail.html)(data);
+
+			this.mailer.sendMail(mail, (error, info) => {
+				if (error) {
+					log.info('Failed to deliver email', error);
+					return reject(error);
+				}
+
+				log.info('Successfully delivered email to %s', mail.to);
+				resolve(info);
+			});
+		});
+	}
+}
