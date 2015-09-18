eacedd20a1156f606de1e9de20e5110d59dec368
boo1ean <boo1ean0807@gmail.com>
2015-09-18 13:49:35 +0300

    Add dependencies, add logging to watchdog

--- src/watchdog.js
+++ src/watchdog.js
@@ -3,6 +3,7 @@
 var _ = require('lodash');
 var Asserter = require('./asserter');
 var Reporter = require('./reporter');
+var log = require('./log');
 
 module.exports = class Watchdog {
 	constructor (opts) {
@@ -11,15 +12,19 @@ module.exports = class Watchdog {
 	}
 
 	assert (repo, commits) {
+		log.info('Start commits asserting');
+
 		return new Promise((resolve, reject) => {
 			var errors = this.asserter.validate(repo, commits);
 
 			// No errors - no worries
 			if (_.isEmpty(errors)) {
+				log.info('Errors not found, good job!');
 				return resolve()
 			}
 
-			resolve(reporter.report(repo, errors));
+			log.info('Unfortunately some errors were found');
+			resolve(this.reporter.report(repo, errors));
 		});
 		
 	}
